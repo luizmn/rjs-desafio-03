292ab46d3297e6dca24812efca6a9d1052f95ee4
luizmn <luizmn@outlook.com>
2021-06-22 15:28:48 +0100

    fixed header cart count

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -8,8 +8,7 @@ import { useCart } from '../../hooks/useCart';
 
 const Header = (): JSX.Element => {
   const { cart } = useCart();
- 
-  const cartSize = cart.reduce( (totalCart, cart) => totalCart + cart.amount, 0);
+  const cartSize = cart.length;
 
   return (
     <Container>
